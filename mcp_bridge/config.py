538bc746e3267d2349e7507decff22f37313c594
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-12-01 19:07:51 +0000

    add config

--- mcp_bridge/config.py
+++ mcp_bridge/config.py
@@ -1,4 +1,4 @@
-from sys import settrace
+from typing import Annotated, Optional, Union
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import AnyHttpUrl, BaseModel, Field
 
@@ -9,10 +9,25 @@ class InferenceServer(BaseModel):
         default="unauthenticated", description="API key for the inference server"
     )
 
+class STDIOMCPServer(BaseModel):
+    # this is mostly copied from claude desktop
+    command: str = Field(..., description="Command to run the MCP server")
+    args: list[str] = Field(default_factory=list, description="Arguments to pass to the MCP server")
+    env: Optional[dict[str, str]] = Field(None, description="Environment variables to pass to the MCP server")
+
+class SSEMCPServer(BaseModel):
+    # TODO: expand this once I find a good definition for this
+    url: str = Field(description="URL of the MCP server")
+
+MCPServer = Annotated[Union[STDIOMCPServer, SSEMCPServer], Field(description="MCP server configuration")]
 
 class Settings(BaseSettings):
     inference_server: InferenceServer = Field(
         description="Inference server configuration"
+    )
+
+    mcp_servers: dict[str, MCPServer] = Field(
+        description="MCP servers configuration"
     )
 
     model_config = SettingsConfigDict(
