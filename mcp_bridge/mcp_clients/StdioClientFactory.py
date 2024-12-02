8c38fd3916d0fc1060299e770b82e1fe10038d8b
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-12-02 14:27:32 +0000

    fix hanging issue and improve environment handling

--- mcp_bridge/mcp_clients/StdioClientFactory.py
+++ mcp_bridge/mcp_clients/StdioClientFactory.py
@@ -1,15 +1,25 @@
-from mcp import StdioServerParameters, stdio_client
+from mcp import ClientSession, StdioServerParameters, stdio_client
 from .ClientInstance import ClientInstance
 from loguru import logger
+import shutil
+import os
 
 async def construct_stdio_server(config: StdioServerParameters) -> ClientInstance:
     logger.log("DEBUG", "Constructing Stdio Server")
+
+    env = dict(os.environ.copy())
+
+    if config.env is not None:
+        env.update(config.env)
+
     server_parameters = StdioServerParameters(
-        command=config.command,
+        command=shutil.which(config.command),
         args=config.args,
-        env=config.env,
+        env=env,
     )
 
     
     async with stdio_client(server_parameters) as client:
-        return ClientInstance(name="stdio", server=client)
+        async with ClientSession(*client) as session:
+            await session.initialize()
+            return ClientInstance(name="stdio", server=session)
